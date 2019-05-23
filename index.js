4e948ef56ba6ef1cdbf8024b6ea76022e412a7ff
Tosca Yoel Connery <toscayoel.connery@webimp.com.sg>
2019-05-23 15:01:27 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+require('./services/passport');
+ 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: 'there'
-    });
-});
+require('./routes/authRoutes.js')(app);
+
+
+// app.get('/', (req, res) => {
+//     res.send({
+//         hi: 'there'
+//     });
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
